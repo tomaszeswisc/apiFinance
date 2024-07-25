f355b37f4f454e7d70b322bb2e77ee2d062c49b5
hiltonelias <tomaszeswisc@gmail.com>
2024-07-24 23:44:51 -0300

    Conectando ao finance_db

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
-//Importar as Bibliotecas
 
-const express = require('express'); //importa o framework Express
-const dotenv = require('dotenv'); //importa o pacote dotenv para gerenciar variáveis de ambiente
-const cors = require('cors'); //importa o pacote cors para permitir requisiçoes de diferentes origens
-const bodyParser = require('body-parser'); //importa o pacote body-parser para analisar o corpo das requisiçoes HTTP
-
+const dotenv = require('dotenv'); // Importa o pacote dotenv para gerenciar variáveis de ambiente
 
 //Configurar as Variáveis de ambiente
 
 dotenv.config(); // Carrega as variáveis definidas no arquivo '.env' para process.env(processos)
 
+//Importar as Bibliotecas
+const express = require('express'); // Importa o framework Express
+const cors = require('cors'); // Importa o pacote cors para permitir requisições de diferentes origens
+const bodyParser = require('body-parser'); // Importa o pacote body-parser para analisar o corpo das requisições HTTP
+
+const db = require('./config/db'); // Importa a conexão com o banco de dados
+
 
 //inicializar nova aplicação Express
 
-const app = express(); 
+const app = express(); // Inicializa uma nova aplicação Express
 
 
 //configurar o CORS e o bady-Parse
 
-app.use(cors()); //habilita o cors para todas as rotas
-app.use(bodyParser.json()); // configura o body-parser para analisar requisições JSON
+app.use(cors()); // Habilita o CORS para todas as rotas
+app.use(bodyParser.json()); // Configura o body-parser para analisar requisições JSON
 
 
 //Rota inicial para testar o servidor
 
-app.get('/',(req, res)=> {
-    res.send("Servidor está rodando"); //difinir uma rota para testar o servidor
-});
+app.get('/', (req, res) => {
+    res.send('Servidor está rodando'); // Define uma rota inicial para testar o servidor
+  });
 
 //Configurar o servidor para uma porta específica
 
-const PORT = process.env.PORT || 3000; // Define a porta a partir da variavel de ambiente ou usa a porta 3000 como padrão
-app.listen(PORT,()=> {
-    console.log(`Servidor rodando na porta  ${PORT}`); //Envia uma mensagem informando que o servidor esta rodando na '$PORT' em que estiver no momento
+const PORT = process.env.PORT || 3000; // Define a porta a partir da variável de ambiente ou usa a porta 3000 como padrão
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`); // Loga uma mensagem informando que o servidor está rodando
 });
